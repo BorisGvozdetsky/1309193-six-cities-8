27f80821813d82627ed09f455be4bd655eb06cbb
BorisGvozdetsky <borisgvozdetsky@gmail.com>
2021-10-03 14:49:53 +0300

    Adds first React components

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  PLACE_COUNT: 5,
+  OFFER_COUNT: 312,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App placeCount = {Setting.PLACE_COUNT} offerCount = {Setting.OFFER_COUNT}/>
   </React.StrictMode>,
   document.getElementById('root'));
